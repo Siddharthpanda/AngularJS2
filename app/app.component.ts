7940567a5210af31346e2285a3dc9e440c9ef039
siddharthpanda <siddharth.panda@navaratan.com>
2016-10-04 20:20:41 +0530

    Angular second day

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,15 @@
 import {Component} from 'angular2/core';
 import {MediaItemListComponent} from './media-item-list.component';
+import {MediaItemFormComponent} from './media-item-form.component';
+import {RouteConfig} from 'angular2/router';
 
+@RouteConfig([
+    { path: '/:medium', component: MediaItemListComponent, name: 'List' },
+    { path: '/add', component: MediaItemFormComponent, name: 'AddMediaItem' }
+])
 @Component({
     selector: 'media-tracker-app',
-    directives: [MediaItemListComponent],
+    directives: [MediaItemListComponent, MediaItemFormComponent],
     templateUrl: 'app/app.component.html',
     styleUrls: ['app/app.component.css']
 })
